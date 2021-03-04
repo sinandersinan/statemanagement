baf4a62b732901fdb05d5dec25d90a6c0aaf0d8e
sinanyuce <sinan.yuece@gmail.com>
2021-03-04 11:39:25 +0100

    adding bootstrap and dependencies to work properly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {StateService} from './state.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'statemanagement';
+
+  constructor(public service: StateService) {
+  }
 }
